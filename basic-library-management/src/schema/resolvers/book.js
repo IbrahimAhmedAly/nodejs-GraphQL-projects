49b98a8c3f890d9ee91830fbb3bcc1a6dca77afb
IbrahimAhmedAly <ibrahim.zagglol@gmail.com>
2025-01-03 20:49:36 +0200

    add pagination, filtering, and sorting to books query resolver

--- basic-library-management/src/schema/resolvers/book.js
+++ basic-library-management/src/schema/resolvers/book.js
@@ -6,11 +6,46 @@ const bookResolvers = {
   Date: DateScalar,
 
   Query: {
-    books: async () => await Book.find(),
+    books: async (
+      _,
+      { page = 1, limit = 10, genre, yearFrom, yearTo, searchTitle }
+    ) => {
+      const skip = (page - 1) * limit;
+      let query = {};
+
+      if (genre) query.genre = genre;
+      if (yearFrom || yearTo) {
+        query.publishedYear = {};
+        if (yearFrom) query.publishedYear.$gte = yearFrom;
+        if (yearTo) query.publishedYear.$lte = yearTo;
+      }
+      if (searchTitle) {
+        query.title = { $regex: searchTitle, $options: "i" };
+      }
+
+      const books = await Book.find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort({ publishedYear: -1 });
+
+      const total = await Book.countDocuments(query);
+
+      return {
+        books,
+        pageInfo: {
+          total,
+          pages: Math.ceil(total / limit),
+          currentPage: page,
+          hasNext: skip + books.length < total,
+        },
+      };
+    },
     book: async (_, { id }) => await Book.findById(id),
   },
   Book: {
     author: async (book) => await Author.findById(book.authorId),
+    publishedDate: (book) =>
+      book.publishedYear ? new Date(book.publishedYear, 0) : null,
   },
   Mutation: {
     createBook: async (_, { input }) => {
