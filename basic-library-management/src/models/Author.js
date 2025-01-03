94cc90e53d4af71e78605e2a18d0820171e06adb
IbrahimAhmedAly <ibrahim.zagglol@gmail.com>
2025-01-03 16:11:22 +0200

    add Author and User models with Mongoose schemas

---
+++ basic-library-management/src/models/Author.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Author", authorSchema);
